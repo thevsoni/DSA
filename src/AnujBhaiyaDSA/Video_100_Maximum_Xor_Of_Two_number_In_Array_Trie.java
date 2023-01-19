9d61ada0c6f62281ed5f151f517a328d3094ae1f
Vishal Soni <vs332837@gmail.com>
2023-01-19 09:56:40 +0530

    DSA

--- src/AnujBhaiyaDSA/Video_100_Maximum_Xor_Of_Two_number_In_Array_Trie.java
+++ src/AnujBhaiyaDSA/Video_100_Maximum_Xor_Of_Two_number_In_Array_Trie.java
@@ -1,6 +1,6 @@
 package AnujBhaiyaDSA;
 
-public class Video_100 {
+public class Video_100_Maximum_Xor_Of_Two_number_In_Array_Trie {
     public static void main(String[] args) {
 
     }
--- src/leetcode/Question.java
+++ src/leetcode/Question.java
@@ -12,44 +12,10 @@ import java.io.*;
 import java.util.stream.IntStream;
 import java.util.Collections;
 
-class Solution {
-    public int countStudents(int[] students, int[] sandwiches) {
-        Stack<Integer> stack = new Stack<>();
-        Queue<Integer> queue = new LinkedList<>();
-
-        for(int i=0; i<students.length; i++){
-            queue.add(students[i]);
-        }
-        for(int i=sandwiches.length-1; i>=0; i--){
-            stack.push(sandwiches[i]);
-        }
-
-        int ans = 0;
-        int temp;
-        while(!queue.isEmpty()){
-            if(queue.peek()==stack.peek()){
-                queue.remove();
-                stack.pop();
-            }else{
-                if(!stack.contains(queue.peek())){
-                    ans = queue.size();
-                    break;
-                }else{
-                    queue.add(queue.remove());
-                }
-            }
-        }
-        return ans;
-    }
-}
 public class Question {
 
     public static void main(String[] args) {
-       int[] student = {1,1,1,0,0,1};
-       int[] sandwiches = {1,0,0,0,1,1};
 
-       Solution s = new Solution();
-        System.out.println(s.countStudents(student,sandwiches));
-
+        System.out.println((char)('a'+2));
     }
 }
