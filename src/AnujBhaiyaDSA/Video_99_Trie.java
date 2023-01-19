11f3f12054dccdd4b133dbf9641a84a87d2a16ca
Vishal Soni <vs332837@gmail.com>
2023-01-19 07:51:49 +0530

    DSA

--- src/AnujBhaiyaDSA/Video_99_Trie.java
+++ src/AnujBhaiyaDSA/Video_99_Trie.java
@@ -1,7 +1,16 @@
 package AnujBhaiyaDSA;
 
+import java.util.TreeMap;
+
 public class Video_99_Trie {
+    static class TrieNode{
+        TrieNode[] children;
+        boolean isEndOfWord;
+        TrieNode(){
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hi");
     }
 }
